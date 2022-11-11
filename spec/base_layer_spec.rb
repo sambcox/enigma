359d927fba70951dc5e4c2c72cfdd246ba996e7f
Samuel Cox <samc1253@gmail.com>
2022-11-10 22:40:02 -0700

    Add test for making a random key

--- spec/base_layer_spec.rb
+++ spec/base_layer_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe BaseLayer do
     expect(enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
   end
 
+  it 'can create a random key if no key is passed in' do
+    expect(enigma.random_key).to be_a String
+    expect(enigma.random_key.length).to eq 5
+  end
+
   it 'can get the information from the date needed to encrypt' do
     expect(enigma.date_mover("052797")).to eq([3, 2, 0, 9])
   end
