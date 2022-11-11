5d8fc1b44a52a06dc86c72883c633cf03a99eded
Samuel Cox <samc1253@gmail.com>
2022-11-10 19:40:53 -0700

    Add foundation of encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,18 @@
+require 'date'
+
 class Enigma
-  def initialize
-    
+  def encrypt(to_be_encrypted, key, date = Date.today.to_s.delete('-'))
+    alphabet = Array('a'..'z').push(' ')
+    date_mover = (date.to_i ** 2).digits.reverse.last(4)
+    key_split = key.split('')
+    key_1 = (key_split.first(2).join.to_i) + date_mover.first
+    as_arr = to_be_encrypted.chars
+    encrypted = []
+    as_arr.each_with_index do |letter, index|
+      encrypted << alphabet[alphabet.index(letter) + key_1] if (index % 4) == 0
+      encrypted << alphabet[alphabet.index(letter) + key_1] if (index % 4) == 1
+      encrypted << alphabet[alphabet.index(letter) + key_1] if (index % 4) == 2
+      encrypted << alphabet[alphabet.index(letter) + key_1] if (index % 4) == 3
+    end
   end
 end
