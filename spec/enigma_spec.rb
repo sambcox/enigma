1637fc370d4dcc8bedd0e1e55c1d5568704ff1a9
Samuel Cox <samc1253@gmail.com>
2022-11-10 18:11:06 -0700

    Add test for encrypt method

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,8 +1,16 @@
 require './lib/enigma'
 
 RSpec.describe Enigma do
+  let(:enigma) { Enigma.new}
   it 'exists' do
-    enigma = Enigma.new
     expect(enigma).to be_a Enigma
+  end
+
+  it 'can encrypt a string' do
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    })
   end
 end
