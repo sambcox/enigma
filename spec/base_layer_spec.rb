ca2b7dd8e483024e07bfc1d9dac5bea31a623f92
Samuel Cox <samc1253@gmail.com>
2022-11-10 22:00:16 -0700

    Add test for finding the mover for the date

--- spec/base_layer_spec.rb
+++ spec/base_layer_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe BaseLayer do
   it 'can create an alphabet' do
     expect(enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
   end
+
+  it 'can get the information from the date needed to encrypt' do
+    expect enigma.date_mover("052797").to eq([3, 2, 0, 9])
+  end
 end
