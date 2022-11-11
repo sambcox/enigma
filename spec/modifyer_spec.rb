fc96e639791d3eca3f7d8aca21ee89a809ea49c2
Samuel Cox <samc1253@gmail.com>
2022-11-10 22:55:57 -0700

    Add first test for modifying the character

---
+++ spec/modifyer_spec.rb
@@ -0,0 +1,11 @@
+require './lib/enigma'
+require './lib/base_layer'
+require './lib/modifyer'
+
+RSpec.describe Modifyer do
+  let(:enigma) { Enigma.new }
+  it 'can find what index to make the character' do
+    expect(new_character('h', 1)).to eq('j')
+    expect(new_character(' ', 23)).to eq('w')
+  end
+end
