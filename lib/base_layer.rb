858779f2cd3c1ce6e617eb12ed00f7fbc1a72dc0
Samuel Cox <samc1253@gmail.com>
2022-11-10 22:37:16 -0700

    Add method to return amount by which to move each character

--- lib/base_layer.rb
+++ lib/base_layer.rb
@@ -6,4 +6,14 @@ module BaseLayer
   def date_mover(date)
     (date.to_i ** 2).digits.reverse.last(4)
   end
+
+  def key_maker(key, date)
+    date_moved = date_mover(date)
+    key_split = key.split('')
+    key_made = []
+    key_made << ((key_split.first(2).join.to_i) + date_moved.first) % 27
+    key_made << ((key_split[1, 2].join.to_i) + date_moved[1]) % 27
+    key_made << (([key_split[2], key_split[3]].join.to_i) + date_moved[2]) % 27
+    key_made << ((key_split[3, 4].join.to_i) + date_moved[3]) % 27
+  end
 end
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,16 +4,14 @@ require_relative 'base_layer'
 class Enigma
   include BaseLayer
   def encrypt(to_be_encrypted, key, date = Date.today.to_s.delete('-'))
-    date_parsed = date_mover(date)
-    key_split = key.split('')
-    key_1 = (key_split.first(2).join.to_i) + date_parsed.first
+    key_made = key_maker(key, date)
     as_arr = to_be_encrypted.chars
     encrypted = []
     as_arr.each_with_index do |letter, index|
-      encrypted << alphabet[alphabet.index(letter) + key_1] if (index % 4) == 0
-      encrypted << alphabet[alphabet.index(letter) + key_1] if (index % 4) == 1
-      encrypted << alphabet[alphabet.index(letter) + key_1] if (index % 4) == 2
-      encrypted << alphabet[alphabet.index(letter) + key_1] if (index % 4) == 3
+      encrypted << alphabet[alphabet.index(letter) + key_made[0]] if (index % 4) == 0
+      encrypted << alphabet[alphabet.index(letter) + key_made[1]] if (index % 4) == 1
+      encrypted << alphabet[alphabet.index(letter) + key_made[2]] if (index % 4) == 2
+      encrypted << alphabet[alphabet.index(letter) + key_made[3]] if (index % 4) == 3
     end
   end
 end
