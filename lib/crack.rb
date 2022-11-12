472cc36bd166e92d4c79fb781ac2a85031958250
Samuel Cox <samc1253@gmail.com>
2022-11-11 18:44:34 -0700

    Add command line functionality to crack method

---
+++ lib/crack.rb
@@ -0,0 +1,8 @@
+require_relative 'enigma'
+
+read_file = ARGV[0].dup.prepend('./lib/')
+write_file = ARGV[1].dup.prepend('./lib/')
+enigma = Enigma.new
+cracked = enigma.crack(File.read(read_file), ARGV[2])
+File.write(write_file, cracked[:decryption])
+puts "Created #{ARGV[1]} with the cracked key #{cracked[:key]} and date #{cracked[:date]}"
